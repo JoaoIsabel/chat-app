4348a86467abd2901a8686e91835d1ed6e892a90
JoaoIsabel <joao.alves.isabel@gmail.com>
2020-10-14 09:52:46 +0100

    chat-app creation

---
+++ src/components/contact.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./contact.css";
+
+function Contact(){
+    return(
+        <div className = "Contact">
+            
+            <img className = "avatar" src = "https://randomuser.me/api/portraits/women/9.jpg"/>
+            
+            <div className = "name">
+                Jacinto Pinto                
+            </div>
+            <div className = "status">
+                <div className = "status-text">
+                    <div className = "status-online">
+                        on-line
+                    </div>
+        
+
+
+                </div>
+
+            </div>
+        </div>
+    )
+
+}
+
+
+export default Contact;
